0354eda912114a37dfe60facac08dc3f351a7918
Allen Welkie <allen@localhost.localdomain>
2014-06-07 10:28:54 -0400

    Added a scoring function, with commented-out options for parallelism

--- labs.hs
+++ labs.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Control.Parallel.Strategies
 
 -- Performs half of an autocorrelation (only positive lags)
 autocorr :: [Int] -> [Int]
@@ -12,15 +13,25 @@ bin_seqs :: Int -> [[Int]]
 bin_seqs n = iterate add_bit [[]] !! n
     where add_bit seqs = [b : seq | seq <- seqs, b <- [-1,1]]
 
+score_seqs :: [[Int]] -> [(Int, [Int])]
+-- without parallelism
+score_seqs = map (\x -> (score x, x))
+
+-- With parallelism, but not lazy
+--score_seqs = parMap rdeepseq (\x -> (score x, x))
+
+-- With parallesism, and lazy (will not eat your ram)
+--score_seqs seqs = withStrategy (parBuffer 100000 rdeepseq) (map (\x -> (score x, x)) seqs)
+
 -- Find the set of binary sequences that has the lowest score.
 labs :: [[Int]] -> [[Int]]
-labs seqs = prune seqs (score (head seqs)) []
-    where prune [] _ best_seqs = best_seqs
-          prune (x:xs) min best_seqs = case (compare this_score min) of
-                                        LT -> prune xs this_score [x]
+labs seqs = prune scored_seqs (score (head seqs)) []
+    where scored_seqs = score_seqs seqs
+          prune [] _ best_seqs = best_seqs
+          prune ((score,x):xs) min best_seqs = case (compare score min) of
+                                        LT -> prune xs score [x]
                                         GT -> prune xs min best_seqs
                                         EQ -> prune xs min (x:best_seqs)
-                                        where this_score = score x
 
 main :: IO ()
 main = do
